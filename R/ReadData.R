ca8a81e720a287de1bb2bfe7640417277cad9fc7
JaspersStijn <82697311+JaspersStijn@users.noreply.github.com>
2023-11-23 12:03:50 +0100

    Update ReadData.R

--- R/ReadData.R
+++ R/ReadData.R
@@ -6,3 +6,6 @@ source("./R/Main_Function.R")
 
 ## Read in data
 dat = read_xlsx("./Data/Pyridine.xlsx")
+dat = dat %>% mutate(Dose = as.factor(Dose),
+                     Sex = factor(Sex,labels=c("M","F")),
+                     weight_use = as.numeric(weight_use))
